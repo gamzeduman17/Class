e22d7106fe762734e7672227fae29292bd7e5969
Gamze Duman <gamzeduman1729@gmail.com>
2023-03-05 00:46:33 +0300

    İLK PROJEMSİİİİİ

---
+++ Class/SortedList/MUSTERILER.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    public class MUSTERILER
+    {
+        public string kullanici_ad;
+        public string mail;
+        public int tel_no;
+        public int sifre;
+        public int dogum_yili;
+        public string sehir;
+        public MUSTERILER(string kullanici_ad, string mail, int tel_no, int sifre, int dogum_yili, string sehir)
+        {
+            this.kullanici_ad = kullanici_ad;
+            this.mail = mail;
+            this.tel_no = tel_no;
+            this.sifre = sifre;
+            this.tel_no = tel_no;
+            this.dogum_yili= dogum_yili;
+        }
+
+
+
+
+
+    }
+}
---
+++ Class/SortedList/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList musteri1= new ArrayList(); 
+            ArrayList musteri2= new ArrayList();
+            Console.WriteLine("HOŞGELDİNİZ");
+            Console.WriteLine("GİRİŞ YAPMAK İÇİN 1'E BASINIZ.");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("KAYITLI KULLANICI DEĞİLSENİZ KAYIT OLMAK İSTER MİSİNİZ??? \n EVET için E \n HAYIR için H");
+           
+            string secim=Convert.ToString(Console.ReadLine());
+            if(secim=="1")
+            { Console.WriteLine("LÜTFEN KULLANICI ADI VE ŞİFRE GİRİNİZ. \n Kullanıcı Adı: \n Şifre:"); 
+                string kullanici_adi=Convert.ToString(Console.ReadLine());  
+                int sifre=Convert.ToInt32(Console.ReadLine());
+
+                if(kullanici_adi=="GAMZE" && sifre==1234)
+                {
+
+                    Console.WriteLine("BAŞARILI BİR ŞEKİLDE GİRİŞ YAPTINIZ. \n ŞİMDİ HANGİ İŞLEMİ YAPMAK İSTİYORSUNUZ? ");
+                    Console.WriteLine("1.MAIL ADRESİ GÜNCELLEME \n 2.SIFRE GUNCELLEME \n 3.TEL NO" +
+                        " GÜNCELLEME \n 4.ADRES GÜNCELLEME \n 5.MUSTERI PUANI HESAPLAMA");
+                    int secim1=Convert.ToInt32(Console.ReadLine());
+                    switch(secim1)
+                    {
+                        case 1: 
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+
+
+
+                }
+            
+             else if(secim.ToLower()=="E")
+
+                {
+                    Console.WriteLine("LÜTFEN KULLANICI ADI VE ŞİFRE BELİRLEYİNİZ. \n Kullanıcı Adı: \n Şifre:");
+                    string kullanici_adi1 = Convert.ToString(Console.ReadLine());
+                    int sifre1 = Convert.ToInt32(Console.ReadLine());
+
+                    musteri1.Add(kullanici_adi1);
+                    musteri2.Add(sifre);//bu ikisini ve diğer bilgileri nasıl aynı listeye atabilirim????
+
+
+
+                }
+
+                else
+                {
+                    Console.WriteLine("HOŞÇAKALIN:)");
+                    System.Threading.Thread.Sleep(2000);
+                    
+                }
+            
+            }
+
+
+
+        }
+    }
+}
