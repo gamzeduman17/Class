98b73a918356cd830deab24373e7f31017e8f88b
Gamze Duman <gamzeduman1729@gmail.com>
2023-03-08 11:59:27 +0300

    Yapıcı metot örnekleme

---
+++ OOP1/OOP1/Arac.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Arac
+    {
+
+        public string marka;
+        public string model;
+        public int model_yil;
+        public decimal km;
+        public int yakit_tipi;
+        public int vites_tipi;
+        public int alis_fiyati;
+        public decimal satis_fiyati;
+        public decimal maxIndirim_tutari;
+        public int fiyat;
+
+
+        public Arac() { }//yapıcı metot
+
+
+        public Arac(string marka, string model, int model_yil,decimal km)
+        {
+            this.marka= marka;
+            this.model= model;  
+            this.model_yil= model_yil;
+            this.km= km;
+     
+        }
+        public void BilgileriGoruntule()
+        {
+            Console.WriteLine("ARAÇ MARKASI:" +marka);
+            Console.WriteLine("ARAÇ MODEL:" + model);
+            Console.WriteLine("ARAÇ MODEL YILI:" + model_yil);
+
+        }
+
+        public void FiyatAta(int _fiyat)
+        {
+
+            int FiyatHesabi = satis_fiyati - maxIndirim_tutari;
+            if(_fiyat<FiyatHesabi)
+            {
+                Console.WriteLine("GEÇERSİZ FİYAT GİRDİNİZ!");
+
+
+
+            }
+
+            else
+            {
+                this.fiyat = _fiyat;//yeni fiyat ata
+                Console.WriteLine("FİYATINIZ GÜNCELLENDİ.");
+
+            }
+        }
+           
+    }
+}
---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Arac a1 = new Arac("OPEL", "CORSA", 2010, 560000);
+            a1.alis_fiyati = 280000;
+            a1.satis_fiyati = 350000;
+            a1.maxIndirim_tutari = 1500;
+            a1.fiyat =//FİYATI DEĞİŞEMİYORUZ CAPSULLEME KONUSUUU!!!!
+
+            a1.FiyatAta(30000);
+            a1.BilgileriGoruntule();
+            Console.ReadKey();
+
+
+        }
+    }
+}
